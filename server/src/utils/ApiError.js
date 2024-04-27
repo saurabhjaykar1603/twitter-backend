0faa83787a1c4a6233fb51bfd4d1851c8f31830b
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-04-28 02:24:16 +0530

    feat: add ApiError class for structured error handling
    Implement the ApiError class extending the native Error class to include additional properties such as statusCode, message, success, errors, and optional data. This enhancement improves error management by providing more detailed context in API responses and error logs.

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    data = null,
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+    this.data = data;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
