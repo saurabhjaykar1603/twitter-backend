fa49877f856927d0e28305edbfade72911d4dae3
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-04-28 15:11:22 +0530

    feat : user model defined

---
+++ server/src/models/user.model.js
@@ -0,0 +1,59 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLenght: 6,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    following: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    links: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+export default User;
