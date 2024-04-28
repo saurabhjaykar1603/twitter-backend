63234be55e9d34d30b865057e18c57791a817c65
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-04-28 15:13:01 +0530

    Add express.json() middleware and update import statement
    - One import statement for authRoutes has been updated.
    - A new middleware for parsing JSON request bodies has been added to the app instance.

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 const app = express();
+app.use(express.json());
 
+
+
+
+
+// routes
 app.use("/api/v1/auth", authRoutes);
 
 export { app };
