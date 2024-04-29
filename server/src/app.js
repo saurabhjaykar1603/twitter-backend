dd7396c2d5989d58ad20952216ca27ca7f09791a
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-04-29 15:57:49 +0530

    chore : "Use express.urlencoded middleware to parse form data; clean up imports"
    This commit adds the `express.urlencoded` middleware to parse form data in the application. It also removes unnecessary blank lines and imports the `

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,9 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 app.use(express.json());
-
-
-
-
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 // routes
 app.use("/api/v1/auth", authRoutes);
