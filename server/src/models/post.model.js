0a2dc0e271965bfa265eae5f39f2f085b3a37e3d
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-05-21 21:06:48 +0530

    feat(model) : post model added

---
+++ server/src/models/post.model.js
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    likes: [
+      {
+        text: {
+          type: String,
+          required: true,
+        },
+        user: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+      },
+    ],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+const Post = model("Post", postSchema);
+export default Post;
