1418e65875f4ed462d3263d4fbb3fb60f1b7c5bf
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-05-22 14:20:13 +0530

    feat(api) : add comment on post api endpoint added

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -4,6 +4,7 @@ import Post from "../models/post.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { v2 as cloudinary } from "cloudinary";
 import User from "../models/user.model.js";
+import mongoose from "mongoose";
 
 // Controller function to create a new post
 const createPost = asyncHandler(async (req, res) => {
@@ -82,4 +83,49 @@ const deletePost = asyncHandler(async (req, res) => {
     .json(new ApiResponse(204, null, "Post deleted successfully"));
 });
 
-export { createPost, deletePost };
+const commentOnPost = asyncHandler(async (req, res) => {
+  // Extracting the post ID from the request parameters
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ApiError(400, "Invalid post ID");
+  }
+  // Extracting the comment text from the request body
+  const { text } = req.body;
+
+  // Getting the ID of the currently logged-in user from the request object
+  const loggedInUserId = req.user._id;
+
+  // Checking if the comment text is provided in the request body
+  if (!text) {
+    // Throwing an error if the text field is missing
+    throw new ApiError(400, "Comment text is required");
+  }
+
+  // Finding the post by ID in the database
+  const post = await Post.findById(id);
+
+  // Checking if the post exists
+  if (!post) {
+    // Throwing an error if the post is not found
+    throw new ApiError(404, "Post not found");
+  }
+
+  // Creating a comment object with the provided text and the ID of the current user
+  const newComment = {
+    text,
+    user: loggedInUserId,
+  };
+
+  // Adding the comment object to the comments array of the post
+  post.comments.push(newComment);
+
+  // Saving the updated post back to the database
+  await post.save();
+
+  // Returning a success response with the added comment
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newComment, "Comment added successfully"));
+});
+
+export { createPost, deletePost, commentOnPost };
--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { createPost, deletePost } from "../controllers/post.controller.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+} from "../controllers/post.controller.js";
 import { validateDeletePost } from "../middlewares/validateDeletePost.js";
 import { deletePostRateLimiter } from "../middlewares/deletePostRateLimiter.js";
 
@@ -14,5 +18,6 @@ router.delete(
   validateDeletePost,
   deletePost
 );
+router.post("/comment/:id", authMiddleware, commentOnPost);
 
 export default router;
